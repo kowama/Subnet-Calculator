fd85b1d1938cbefd71d93e95af374a98d57bd2cd
kowama <kowama.nimina@gmail.com>
2019-01-21 19:59:59 +0100

    Net config Alert Dialog setup

--- app/src/main/java/com/nimina/kowama/calculatornetadmin/fragments/VlsmFragment.java
+++ app/src/main/java/com/nimina/kowama/calculatornetadmin/fragments/VlsmFragment.java
@@ -1,19 +1,111 @@
 package com.nimina.kowama.calculatornetadmin.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nimina.kowama.calculatornetadmin.R;
+import com.nimina.kowama.calculatornetadmin.algorithms.NetworkManager;
+import com.nimina.kowama.calculatornetadmin.fragments.dialog.NetConfigDialog;
 
-public class VlsmFragment extends Fragment {
+import java.util.HashMap;
+import java.util.List;
+
+public class VlsmFragment extends Fragment implements NetConfigDialog.NetConfigDialogListener {
+    private EditText[] mIpAddressEditText;
+    private EditText mIpMaskEditText;
+    private SeekBar mIpMaskSeekBar;
+    private TextView mResNetworkTextView;
+    private TextView mResMaskTextView;
+    private TextView mResHostsRangeTextView;
+    private TextView mResBroadcastTextView;
+    private TextView mResNetSizeTextView;
+    private TableLayout mResultsTableLayout;
+
+    private HashMap<String,Integer> mSubNetsMap = new HashMap<>();
+    private List<NetworkManager.Subnet> mResultSubnets;
+
+
+
+    private void initViews(View rootView){
+        mIpAddressEditText = new EditText[4];
+        mIpAddressEditText[0] = rootView.findViewById(R.id.ipPartAEditText);
+        mIpAddressEditText[1] = rootView.findViewById(R.id.ipPartBEditText);
+        mIpAddressEditText[2] = rootView.findViewById(R.id.ipPartCEditText);
+        mIpAddressEditText[3] = rootView.findViewById(R.id.ipPartDEditText);
+        mIpMaskEditText = rootView.findViewById(R.id.ipMaskEditText);
+        mIpMaskSeekBar  = rootView.findViewById(R.id.ipMaskSeekBar);
+
+        mResultsTableLayout    = rootView.findViewById(R.id.resultTableLayout);
+        mResNetworkTextView    = rootView.findViewById(R.id.resNetworkTextView);
+        mResMaskTextView       = rootView.findViewById(R.id.resMaskTextView);
+        mResHostsRangeTextView = rootView.findViewById(R.id.resHostsRangeTextView);
+        mResBroadcastTextView  = rootView.findViewById(R.id.resBroadcastTextView);
+        mResNetSizeTextView    = rootView.findViewById(R.id.resNetSizeTextView);
+
+        clearAllViews();
+
+        ImageButton clearImageButton = rootView.findViewById(R.id.clearImageButton);
+        clearImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearAll(v);
+            }
+        });
+
+        Button netConfigButton = rootView.findViewById(R.id.netConfigButton);
+        netConfigButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNetConfigDialog();
+            }
+        });
+
+    }
+
+    private void openNetConfigDialog(){
+        NetConfigDialog netConfigDialog = new NetConfigDialog();
+        netConfigDialog.show(getActivity().getSupportFragmentManager(),"Network Config Dialog");
+    }
+
+    private void clearAllViews(){
+        mResultsTableLayout.setVisibility(View.INVISIBLE);
+
+        for (final EditText ipPartEditText : mIpAddressEditText){
+            ipPartEditText.setText("");
+        }
+        mIpMaskEditText.setText("");
+
+    }
+    public void clearAll(View view){
+        clearAllViews();
+        Snackbar.make(view, "Clear", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_vlsm, container, false);
+        initViews(rootView);
         return rootView;
+    }
+
+    @Override
+    public void applyNetworksMap(HashMap<String, Integer> subNetsMap) {
+        mSubNetsMap = subNetsMap;
+        // TODO: 21/01/2019
+        //vlsm operattion
+        Toast.makeText(getContext(),mSubNetsMap.toString(),Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/nimina/kowama/calculatornetadmin/fragments/dialog/NetConfigDialog.java
@@ -0,0 +1,64 @@
+package com.nimina.kowama.calculatornetadmin.fragments.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.nimina.kowama.calculatornetadmin.R;
+
+import java.util.HashMap;
+
+public class NetConfigDialog extends DialogFragment {
+    HashMap<String, Integer> mSubNetsMap = new HashMap<>(); // [name: size]
+    private NetConfigDialog.NetConfigDialogListener mNetConfigDialogListener;
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.net_config_dialog, null);
+        builder.setView(view)
+                .setTitle(R.string.label_net_conf_d_title)
+                .setNegativeButton(R.string.button_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //
+                    }
+                }).setPositiveButton(R.string.button_positive, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO: 21/01/2019
+                        //fill the subnet list and send it
+                       // mSubNetsMap.put("A",110);
+                        //mSubNetsMap.put("B",60);
+                        //mSubNetsMap.put("C",60);
+                      //  mNetConfigDialogListener.applyNetworksMap(mSubNetsMap);
+                    }
+        });
+        return builder.create();
+    }
+
+    /*
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mNetConfigDialogListener = (NetConfigDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()+ "Must implement NetConfigDialogListener");
+        }
+    }
+    */
+    public interface  NetConfigDialogListener{
+        void applyNetworksMap(HashMap<String,Integer> subNetsMap);
+    }
+}
