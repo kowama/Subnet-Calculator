b1ce2e68582c4e7f89f79c07a9c32348a9f46a7c
kowama <kowama.nimina@gmail.com>
2019-01-27 22:47:25 +0100

    [***] fixing some bug Subnet result for mask value of {0, 31, 32} [+] add wildcard Mask to result

--- app/src/main/java/com/nimina/kowama/calculatornetadmin/fragments/SubnetFragment.java
+++ app/src/main/java/com/nimina/kowama/calculatornetadmin/fragments/SubnetFragment.java
@@ -32,23 +32,25 @@ public class SubnetFragment extends Fragment {
     private TextView mResHostsRangeTextView;
     private TextView mResBroadcastTextView;
     private TextView mResNetSizeTextView;
+    private TextView mResWildcardMaskTextView;
     private TableLayout mResultsTableLayout;
 
-    private void initViews(View rootView){
+    private void initViews(View rootView) {
         mIpAddressEditText = new EditText[4];
         mIpAddressEditText[0] = rootView.findViewById(R.id.ipPartAEditText);
         mIpAddressEditText[1] = rootView.findViewById(R.id.ipPartBEditText);
         mIpAddressEditText[2] = rootView.findViewById(R.id.ipPartCEditText);
         mIpAddressEditText[3] = rootView.findViewById(R.id.ipPartDEditText);
         mIpMaskEditText = rootView.findViewById(R.id.ipMaskEditText);
-        mIpMaskSeekBar  = rootView.findViewById(R.id.ipMaskSeekBar);
+        mIpMaskSeekBar = rootView.findViewById(R.id.ipMaskSeekBar);
 
-        mResultsTableLayout    = rootView.findViewById(R.id.resultTableLayout);
-        mResNetworkTextView    = rootView.findViewById(R.id.resNetworkTextView);
-        mResMaskTextView       = rootView.findViewById(R.id.resMaskTextView);
+        mResultsTableLayout = rootView.findViewById(R.id.resultTableLayout);
+        mResNetworkTextView = rootView.findViewById(R.id.resNetworkTextView);
+        mResMaskTextView = rootView.findViewById(R.id.resMaskTextView);
+        mResWildcardMaskTextView = rootView.findViewById(R.id.resWildcardMaskTextView);
         mResHostsRangeTextView = rootView.findViewById(R.id.resHostsRangeTextView);
-        mResBroadcastTextView  = rootView.findViewById(R.id.resBroadcastTextView);
-        mResNetSizeTextView    = rootView.findViewById(R.id.resNetSizeTextView);
+        mResBroadcastTextView = rootView.findViewById(R.id.resBroadcastTextView);
+        mResNetSizeTextView = rootView.findViewById(R.id.resNetSizeTextView);
 
         clearAllViews();
 
@@ -64,8 +66,8 @@ public class SubnetFragment extends Fragment {
 
     }
 
-    private void setListenerOnEditableViews(){
-        for (int i=0; i<mIpAddressEditText.length; i++){
+    private void setListenerOnEditableViews() {
+        for (int i = 0; i < mIpAddressEditText.length; i++) {
             final int finalI = i;
             mIpAddressEditText[i].addTextChangedListener(new TextWatcher() {
                 @Override
@@ -76,29 +78,28 @@ public class SubnetFragment extends Fragment {
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
                     /*move cursor when reach 3 char*/
-                    if (s.length() >=3){
+                    if (s.length() >= 3) {
                         moveCursorToNext(finalI);
                     }
                 }
 
                 @Override
                 public void afterTextChanged(Editable s) {
-                    if(s.length() >0){
+                    if (s.length() > 0) {
                         try {
 
-                            if (s.length()>0 &&Integer.parseInt(s.toString()) < 0 || Integer.parseInt(s.toString()) > 255){
+                            if (s.length() > 0 && Integer.parseInt(s.toString()) < 0 || Integer.parseInt(s.toString()) > 255) {
                                 mIpAddressEditText[finalI].setTextColor(Color.RED);
                                 mResultsTableLayout.setVisibility(View.INVISIBLE);
-                            }
-                            else{
+                            } else {
                                 mIpAddressEditText[finalI].setTextColor(Color.BLACK);
                                 updateResult();
                             }
-                        }catch (NumberFormatException e){
-                            Log.e("Exception",Log.getStackTraceString(e));
+                        } catch (NumberFormatException e) {
+                            Log.e("Exception", Log.getStackTraceString(e));
 
-                        }catch (Exception e){
-                            Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                         }
 
 
@@ -132,22 +133,24 @@ public class SubnetFragment extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length() > 0){
+                if (s.length() > 0) {
                     try {
-                        if (Integer.parseInt(s.toString()) < 1 || Integer.parseInt(s.toString()) >30){
-                            mIpMaskEditText.setTextColor(Color.RED);
+                        if (Integer.parseInt(s.toString()) < 0 || Integer.parseInt(s.toString()) > 32) {
+                            mIpMaskEditText.setTextColor(Color.rgb(249, 2, 23));
                             mResultsTableLayout.setVisibility(View.INVISIBLE);
-                        }else {
+                        } else if (Integer.parseInt(s.toString()) == 0 || Integer.parseInt(s.toString()) > 30) {
+                            mIpMaskEditText.setTextColor(Color.rgb(242, 114, 2));
+
+                        } else {
                             mIpMaskEditText.setTextColor(Color.BLACK);
                             mIpMaskSeekBar.setProgress(Integer.parseInt(mIpMaskEditText.getText().toString()));
                             updateResult();
                         }
 
-                    }catch (NumberFormatException e){
-                        Log.e("Exception",Log.getStackTraceString(e));
+                    } catch (NumberFormatException ignored) {
 
-                    }catch (Exception e){
-                        Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                     mIpMaskEditText.setSelection(mIpMaskEditText.getText().length());
                 }
@@ -173,10 +176,10 @@ public class SubnetFragment extends Fragment {
         });
     }
 
-    private void moveCursorToNext(int currentIndex){
+    private void moveCursorToNext(int currentIndex) {
         EditText next;
-        if (currentIndex+1 < mIpAddressEditText.length)
-            next = mIpAddressEditText[currentIndex+1];
+        if (currentIndex + 1 < mIpAddressEditText.length)
+            next = mIpAddressEditText[currentIndex + 1];
         else
             next = mIpMaskEditText;
 
@@ -184,54 +187,70 @@ public class SubnetFragment extends Fragment {
         next.requestFocus();
     }
 
-    private void clearAllViews(){
+    private void clearAllViews() {
         mResultsTableLayout.setVisibility(View.INVISIBLE);
 
-        for (final EditText ipPartEditText : mIpAddressEditText){
+        for (final EditText ipPartEditText : mIpAddressEditText) {
             ipPartEditText.setText("");
         }
         mIpMaskEditText.setText("");
 
     }
 
-    private void updateResult(){
+    private void updateResult() {
         try {
-            String ipAddress =  mIpAddressEditText[0].getText().toString()+"."
-                    + mIpAddressEditText[1].getText().toString()+"."
-                    + mIpAddressEditText[2].getText().toString()+"."
-                    + mIpAddressEditText[3].getText().toString()+"/"
+            String ipAddress = mIpAddressEditText[0].getText().toString() + "."
+                    + mIpAddressEditText[1].getText().toString() + "."
+                    + mIpAddressEditText[2].getText().toString() + "."
+                    + mIpAddressEditText[3].getText().toString() + "/"
                     + mIpMaskEditText.getText().toString();
-
             NetworkManager.Subnet mIpNetwork = new NetworkManager.Subnet(ipAddress);
+            int mask = Integer.valueOf(mIpMaskEditText.getText().toString());
 
-            mResNetworkTextView.setText(mIpNetwork.address);
+            String networkAddress = mIpNetwork.address;
+            String networkRange = mIpNetwork.range;
+            String networkBroadcast = mIpNetwork.broadcast;
+            String networkAllocatedSize = String.valueOf(mIpNetwork.allocatedSize);
+            String wildcardMask = NetworkManager.toDecWildCardMask(mask);
+            if (mask == 0) {
+                networkAddress = "0.0.0.0";
+                networkRange = "0.0.0.0 - 255.255.255.254";
+                networkBroadcast = "255.255.255.255";
+
+            } else if (mask > 30) {
+                networkRange = "NONE";
+                networkAllocatedSize = "NONE";
+                networkBroadcast = "NONE";
+            }
+
+            mResNetworkTextView.setText(networkAddress);
             mResMaskTextView.setText(mIpNetwork.decMask);
-            mResHostsRangeTextView.setText(mIpNetwork.range);
-            mResBroadcastTextView.setText(mIpNetwork.broadcast);
-            mResNetSizeTextView.setText(String.valueOf(mIpNetwork.allocatedSize));
+            mResWildcardMaskTextView.setText(wildcardMask);
+            mResHostsRangeTextView.setText(networkRange);
+            mResBroadcastTextView.setText(networkBroadcast);
+            mResNetSizeTextView.setText(networkAllocatedSize);
 
             mResultsTableLayout.setVisibility(View.VISIBLE);
             //** hide Keyboard ?
 
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             /*-----expected EXCEPTION -------*/
             mResultsTableLayout.setVisibility(View.INVISIBLE);
-        }catch (Exception e){
-            Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-
-
 
 
     }
 
-    public void clearAll(View view){
+    public void clearAll(View view) {
         clearAllViews();
         Snackbar.make(view, "Clear", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
     }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_subnet, container, false);
         initViews(rootView);
         return rootView;
